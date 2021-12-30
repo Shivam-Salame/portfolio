af418255d4b66cd4f78a7761327272b86f94244a
Inferno <shivam8463030933@gmail.com>
2021-12-30 20:18:45 +0530

    Starting with new design

--- file.js
+++ file.js
@@ -1,2 +1,12 @@
-var link = document.querySelector("img").innerHTML="images/2.jpg";
-console.log(link);
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/square.jpg') {
+      myImage.setAttribute('src','images/shivam_1.jpg');
+    } else if(mySrc === 'images/shivam_1.jpg') {
+        myImage.setAttribute('src','images/shivam_2.jpg'); 
+    } else {
+      myImage.setAttribute('src','images/square.jpg');
+    }
+}
