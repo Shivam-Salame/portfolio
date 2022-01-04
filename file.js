c44926ef23ec9463c19aab27deb21fc55b34599f
Inferno <shivam8463030933@gmail.com>
2022-01-04 17:19:08 +0530

    updated

--- file.js
+++ file.js
@@ -1,12 +1,13 @@
+
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/square.jpg') {
+    
+    if(mySrc === 'images/shivam_1.jpg') {
+      myImage.setAttribute('src','images/shivam_2.jpg');    
+    }
+     else {
       myImage.setAttribute('src','images/shivam_1.jpg');
-    } else if(mySrc === 'images/shivam_1.jpg') {
-        myImage.setAttribute('src','images/shivam_2.jpg'); 
-    } else {
-      myImage.setAttribute('src','images/square.jpg');
     }
 }
